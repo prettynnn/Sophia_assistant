1c1603449bcf911aa4b6682246e8cedd7a576dce
if you dont do it <dontlistenthem@protonmail.com>
2026-01-16 23:56:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from routers.main_routers import main_router
-from others.config import log, data
+from asyncio.exceptions import CancelledError
+from others.config import log
+from aiogram import Dispatcher, Bot
 from tokens import api_key
 
-from asyncio.exceptions import CancelledError
-from aiogram import Dispatcher, Bot
-
 import asyncio
-import json
 
 bot = Bot(token=api_key)
 
@@ -15,21 +13,11 @@ dp.include_router(
     main_router
 )
 
-async def save_data():
-    with open('data.json', 'w') as file:
-        json.dump(data, file)
-    await asyncio.sleep(600)
-
 async def main():
-    history = asyncio.create_task(save_data())
     try:
         await dp.start_polling(bot)
     except CancelledError:
-        log(f'gg')
-    except KeyboardInterrupt:
-        log(f'This code is completed!')
-        history.cancel()
-        await history
-
+        pass
+    
 if __name__ == "__main__":
     asyncio.run(main())
