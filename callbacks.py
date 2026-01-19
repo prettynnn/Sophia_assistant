e6e1b4e2d2370e5559991d782120e8b8d94974e1
if you dont do it <dontlistenthem@protonmail.com>
2026-01-19 23:11:55 +0300

    Update and rename routers/test to callbacks.py

---
+++ callbacks.py
@@ -0,0 +1,36 @@
+from aiogram import Router
+from aiogram.types import CallbackQuery
+from aiogram.fsm.context import FSMContext
+
+from others import greetings
+from others.states import User
+from keyboards.reply_keyboards import reply_keyboard
+
+callback_router = Router()
+
+@callback_router.callback_query()
+async def select_language(callback: CallbackQuery, state: FSMContext):   
+    if callback.data == 'lang_en':
+        await callback.message.delete()
+        await callback.message.answer(text=greetings.en,
+                                      reply_markup=reply_keyboard)
+        await state.update_data(user=greetings.en)
+        await state.set_state(User.language)
+    elif callback.data == 'lang_ru':
+        await callback.message.delete()
+        await callback.message.answer(text=greetings.ru,
+                                      reply_markup=reply_keyboard)
+        await state.update_data(user=greetings.ru)
+        await state.set_state(User.language) 
+    elif callback.data == 'lang_zh':
+        await callback.message.delete()
+        await callback.message.answer(text=greetings.zh,
+                                      reply_markup=reply_keyboard)
+        await state.update_data(user=greetings.zh)
+        await state.set_state(User.language) 
+    elif callback.data == 'lang_jp':
+        await callback.message.delete()
+        await callback.message.answer(text=greetings.jp,
+                                      reply_markup=reply_keyboard)
+        await state.update_data(user=greetings.jp)
+        await state.set_state(User.language)
