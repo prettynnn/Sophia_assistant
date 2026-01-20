2b2f91092c22e9adbb90ad93367aacc897478f38
if you dont do it <dontlistenthem@protonmail.com>
2026-01-20 21:03:23 +0300

    Update main_router.py

--- routers/main_router.py
+++ routers/main_router.py
@@ -1,1 +1,15 @@
+from aiogram import Router
 
+from .callbacks import callback_router
+from .fsm import fsm_router
+from .text_routers import text_router
+from .start_routers import start_router
+
+main_router = Router()
+
+main_router.include_routers(
+    start_router,
+    callback_router,
+    fsm_router,
+    text_router
+    )
