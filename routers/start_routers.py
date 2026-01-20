eadee87a4103fb0e08c0af81d5333131e4414a59
if you dont do it <dontlistenthem@protonmail.com>
2026-01-20 21:03:47 +0300

    Update start_routers.py

--- routers/start_routers.py
+++ routers/start_routers.py
@@ -6,45 +6,37 @@ from aiogram.fsm.context import FSMContext
 
 from keyboards.inline_keyboards import inline_keyboard
 from database.memory import connector_to_server, select_data_from_table, include_in_table
-from tokens import api_key
 from others.stickers import stickers
+from database.roles import system
 from others.states import User
 
 import random
 
-bot = Bot(token=api_key)
 start_router = Router()
 
 @start_router.message(Command("start", prefix='/'))
 async def start_handler(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
-    chat_id = message.chat.id
     stick_id = random.choice(stickers)
-    role = None
-    content = None
-    # language_text = '💬 Select language'
         
     connect = await connector_to_server()
     data = await select_data_from_table(connect, 
-                                        user_id,
-                                        role,
-                                        content)
-    
+                                      user_id)
     if user_id not in data:
-        role = 'system'
-        content = 'I a helpful assistant a Sophia'
+        content = 'I am a helpful assistant a Sophia'
         await include_in_table(connect,
                                user_id,
-                               role,
+                               system,
                                content)
         await message.bot.send_chat_action(action=ChatAction.TYPING,
-                                           chat_id=chat_id)
+                                            chat_id=message.chat.id)
+        await message.answer_sticker(sticker=stick_id)
         await message.answer(text='💬 Select language',
                             reply_markup=inline_keyboard)
         await state.set_state(User.language)
     else:
         await message.bot.send_chat_action(action=ChatAction.TYPING,
-                                           chat_id=chat_id)
+                                            chat_id=message.chat.id)
         await message.answer_sticker(sticker=stick_id)
         await message.answer(text='💬 Select language',
                             reply_markup=inline_keyboard)
