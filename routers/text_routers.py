66daed33b852a273d0e2903c84eef0becea45d09
if you dont do it <dontlistenthem@protonmail.com>
2026-01-20 21:04:11 +0300

    Update text_routers.py

--- routers/text_routers.py
+++ routers/text_routers.py
@@ -1,46 +1,51 @@
-from aiogram import types, Router, Bot
-from aiogram.utils.chat_action import ChatActionSender
+from aiogram import types, Router
 from aiogram.enums import ChatAction
 
-from tokens import api_key
 from others.cfg import model, client, log
 from database.roles import user, assistant
+from openai import AuthenticationError, InternalServerError, APITimeoutError
 from database.memory import connector_to_server, include_in_table, select_data_from_table
 
 text_router = Router()
-bot = Bot(token=api_key)
 
 @text_router.message()
 async def request_bot_handler(message: types.Message):
     user_id = message.from_user.id
-    # chat_id = message.chat.id
-    
     connect = await connector_to_server()
-    # users = await select_id_from_table(connect, user_id)
     try:
-        user_content = message.text
-        await include_in_table(connect, user_id, user, user_content)
+        user_request = message.text
+        await include_in_table(connect,
+                               user_id,
+                               user,
+                               user_request)
         await message.bot.send_chat_action(action=ChatAction.TYPING,
                                            chat_id=message.chat.id)
         thinking = await message.answer('Thinking...')
-        
-        context = []
-        data = await select_data_from_table(connect, user_id)
-        
+        data = await select_data_from_table(connect,
+                                            user_id)
+        raw_context = []
         for role, content in data:
-            context.insert(0, 
-                {'role': role,
-                 'content': content}
-                )
+            raw_context.append({'role': role,
+                                'content': content})
+        context = raw_context[::-1]
         text = client.responses.create(model=model,
                                        input=context,
                                        stream=False)
         response = text.output_text
+        
         await message.bot.send_chat_action(action=ChatAction.TYPING,
                                            chat_id=message.chat.id)
         await thinking.edit_text(response)
-        
-        context.clear()
-        await include_in_table(connect, user_id, assistant, response)
-    except Exception as e:
-        log(f'Error - {e}')
+        await include_in_table(connect,
+                               user_id,
+                               assistant,
+                               response)
+        raw_context.clear()
+    except AuthenticationError as x:
+        log(f'Your API token is incorrect, replace his! - {x}')
+    except InternalServerError as y:
+        log(f'Server is overloaded, try again... - {y}')
+        await message.reply(text='Service is overloaded now, try again later!')
+    except APITimeoutError as z:
+        log(f'Server not answers, waiting... - {z}')
+        await message.reply(text='Server not reply for your request, please try again!')
