a4eaaa64287bdb4d8bbc2c1824e677a20e72fa10
if you dont do it <dontlistenthem@protonmail.com>
2026-01-21 22:39:01 +0300

    Update greetings.py

--- others/greetings.py
+++ others/greetings.py
@@ -1,19 +1,23 @@
 en = '''👋 Good morning! ✨ I'm Sophia, your personal assistant
 💛 It's great to meet you!
+
 😊 I'm always ready to answer any questions you may have, recommend some new things to you, or simply keep you company
 🌟 Let's make your day productive and enjoyable together!'''
 
 zh = '''👋 早安! ✨我是索菲亞，您的私人助理
 💛 很高興見到您!
+
 😊 隨時準備解答您的任何疑問，為您推薦一些新鮮事物，或只是陪伴您
 🌟 讓我們一起讓您的一天有效率又愉快'''
 
 ru = '''👋 Доброе утро! ✨ Я София, ваш личный ассистент.
 💛 Рада познакомиться!
+
 😊 Я всегда готова ответить на любые ваши вопросы, порекомендовать что-нибудь новое или просто составить вам компанию.
 🌟 Давайте вместе сделаем ваш день продуктивным и приятным!'''
 
 jp = '''👋 おはようございます！✨あなたのパーソナルアシスタント、ソフィアです。
 💛 お会いできて嬉しいです！
+
 😊 ご質問にお答えしたり、新しいものをおすすめしたり、ただ一緒に過ごしたり、いつでも喜んでお手伝いさせていただきます。
 🌟 一緒に、生産的で楽しい一日を！'''
