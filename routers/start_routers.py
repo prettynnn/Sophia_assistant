9cfa1c2a3caf0e28f006f1a6e57f414b0fd06193
if you dont do it <dontlistenthem@protonmail.com>
2026-01-24 19:47:52 +0300

    Update start_routers.py

--- routers/start_routers.py
+++ routers/start_routers.py
@@ -1,42 +1,48 @@
-from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.enums import ChatAction
-from aiogram import types, Router, Bot
+from aiogram import types, Router
 from aiogram.fsm.context import FSMContext
+from aiogram.exceptions import TelegramConflictError, TelegramBadRequest
 
+from database.db import connector_to_server, select_data_from_table, include_in_table
 from keyboards.inline_keyboards import inline_keyboard
-from database.memory import connector_to_server, select_data_from_table, include_in_table
 from others.stickers import stickers
 from database.roles import system
 from others.states import User
+from others.cfg import log
 
 import random
 
 start_router = Router()
 
-@start_router.message(Command("start", prefix='/'))
-async def start_handler(message: types.Message, state: FSMContext):
+@start_router.message(Command("start", prefix='/!'))
+async def start_handler(message: types.Message, state: FSMContext) -> None:
     user_id = message.from_user.id
     stick_id = random.choice(stickers)
-        
-    connect = await connector_to_server()
-    data = await select_data_from_table(connect, 
-                                      user_id)
-    if user_id not in data:
-        content = 'I am a helpful assistant a Sophia'
-        await include_in_table(connect,
-                               user_id,
-                               system,
-                               content)
-        await message.bot.send_chat_action(action=ChatAction.TYPING,
-                                            chat_id=message.chat.id)
-        await message.answer_sticker(sticker=stick_id)
-        await message.answer(text='💬 Select language',
-                            reply_markup=inline_keyboard)
-        await state.set_state(User.language)
-    else:
-        await message.bot.send_chat_action(action=ChatAction.TYPING,
-                                            chat_id=message.chat.id)
-        await message.answer_sticker(sticker=stick_id)
-        await message.answer(text='💬 Select language',
-                            reply_markup=inline_keyboard)
+    
+    try:
+        connect = await connector_to_server()
+        data = await select_data_from_table(connect, 
+                                            user_id)
+        if user_id not in data:
+            content = 'I am a helpful assistant a Sophia'
+            await include_in_table(connect,
+                                   user_id,
+                                   system,
+                                   content)
+            await message.bot.send_chat_action(action=ChatAction.TYPING,
+                                               chat_id=message.chat.id)
+            await message.answer_sticker(sticker=stick_id)
+            await message.answer(text='💬 Select language',
+                                 reply_markup=inline_keyboard)
+            await state.set_state(User.language)
+        else:
+            await message.bot.send_chat_action(action=ChatAction.TYPING,
+                                                chat_id=message.chat.id)
+            await message.answer_sticker(sticker=stick_id)
+            await message.answer(text='💬 Select language',
+                                 reply_markup=inline_keyboard)
+    except TelegramConflictError as a:
+        log(f'This bot launched somewhere else! - {a}')
+    except TelegramBadRequest as b:
+        log(f'Request was declined a Telegram! - {b}')
