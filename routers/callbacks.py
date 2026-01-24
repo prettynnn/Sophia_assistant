010f309950c95dbebcafcb9e6d0d9ad92f179f77
if you dont do it <dontlistenthem@protonmail.com>
2026-01-24 19:45:13 +0300

    Update callbacks.py

--- routers/callbacks.py
+++ routers/callbacks.py
@@ -8,29 +8,33 @@ from keyboards.reply_keyboards import reply_keyboard
 
 callback_router = Router()
 
-@callback_router.callback_query()
-async def select_language(callback: CallbackQuery, state: FSMContext):   
+@callback_router.callback_query(User.language)
+async def select_language(callback: CallbackQuery, state: FSMContext) -> None:   
     if callback.data == 'lang_en':
+        await state.clear()
+        await state.update_data(user=greetings.en)
         await callback.message.delete()
         await callback.message.answer(text=greetings.en,
                                       reply_markup=reply_keyboard)
-        await state.update_data(user=greetings.en)
-        await state.set_state(User.language)
+        await state.set_state(None)
     elif callback.data == 'lang_ru':
+        await state.clear()
+        await state.update_data(user=greetings.ru)
         await callback.message.delete()
         await callback.message.answer(text=greetings.ru,
                                       reply_markup=reply_keyboard)
-        await state.update_data(user=greetings.ru)
-        await state.set_state(User.language) 
+        await state.set_state(None)
     elif callback.data == 'lang_zh':
+        await state.clear()
+        await state.update_data(user=greetings.zh) 
         await callback.message.delete()
         await callback.message.answer(text=greetings.zh,
                                       reply_markup=reply_keyboard)
-        await state.update_data(user=greetings.zh)
-        await state.set_state(User.language) 
+        await state.set_state(None)
     elif callback.data == 'lang_jp':
+        await state.clear()
+        await state.update_data(user=greetings.jp)
         await callback.message.delete()
         await callback.message.answer(text=greetings.jp,
                                       reply_markup=reply_keyboard)
-        await state.update_data(user=greetings.jp)
-        await state.set_state(User.language)
+        await state.set_state(None)
