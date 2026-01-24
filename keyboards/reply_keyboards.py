4831d92090e385be5260603ebb30c7374d04bb0c
if you dont do it <dontlistenthem@protonmail.com>
2026-01-24 19:43:59 +0300

    Update reply_keyboards.py

--- keyboards/reply_keyboards.py
+++ keyboards/reply_keyboards.py
@@ -1,10 +1,14 @@
 from aiogram import types
 
 button = [
-[types.KeyboardButton(text='🏠 Return to main page'),
- types.KeyboardButton(text='🌏 Change language')]
+[types.KeyboardButton(
+    text='🏠 Return to main page'
+    ),
+ types.KeyboardButton(
+     text='🌏 Change language'
+     )]
 ]
 reply_keyboard = types.ReplyKeyboardMarkup(keyboard=button,
-                                    resize_keyboard=True,
-                                    one_time_keyboard=False,
-                                    input_field_placeholder='Talk to me...')
+                                           resize_keyboard=True,
+                                           one_time_keyboard=False,
+                                           input_field_placeholder='Talk to me...')
